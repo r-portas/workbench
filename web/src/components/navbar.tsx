4474470640c1fff2a5ed9cdd56c68c33e2572423
Roy Portas <royportas@gmail.com>
2026-08-18 12:40:13 +1000

    Bump fontsize

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -9,7 +9,7 @@ import { APP_NAME } from "@/lib/app-config";
 function Navbar() {
   return (
     <header className="flex items-center justify-between gap-4 border-b border-border px-6 py-3">
-      <Link to="/" className="text-sm font-semibold">
+      <Link to="/" className="text-lg font-semibold">
         {APP_NAME}
       </Link>
       <WorkbenchSearch />
